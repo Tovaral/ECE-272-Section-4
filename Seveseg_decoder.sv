32d84e883d4007d63b6d243bd4638c244ffa9a72
Alejandro Tovar <tovaral@oregonstate.edu>
2018-05-07 14:04:20 -0700

    fixed segment decoder

--- Seveseg_decoder.sv
+++ Seveseg_decoder.sv
@@ -2,17 +2,17 @@ module Sevenseg( input logic [3:0] data,
 					output logic [6:0] segments);
 			
 			always_comb
-				case(data)
-					0: segments = 7'b0000001;
-					1: segments = 7'b1001111;
-					2: segments = 7'b0010010;
-					3: segments = 7'b0000110;
-					4: segments = 7'b1001100;
-					5: segments = 7'b0100100;
-					6: segments = 7'b0100000;
-					7: segments = 7'b0001111;
+				case(data) //       GFEDCBA - We set our segments backwords
+					0: segments = 7'b1000000;
+					1: segments = 7'b1111001;
+					2: segments = 7'b0100100;
+					3: segments = 7'b0110000;
+					4: segments = 7'b0011001;
+					5: segments = 7'b0010010;
+					6: segments = 7'b0000010;
+					7: segments = 7'b1111000;
 					8: segments = 7'b0000000;
-					9: segments = 7'b0001100;
+					9: segments = 7'b0010000;
 					default:segments = 7'b1111111;
 				endcase
 		endmodule
